271f20acb68a0c3846de94a12416246076e49f75
Park Jun Hyeon / 4rNe5 <me@4rne5.dev>
2024-11-07 08:41:01 +0900

    Initial Commit

---
+++ Boooom Watch App/BoooomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoooomApp.swift
+//  Boooom Watch App
+//
+//  Created by 4rNe5 on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Boooom_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
